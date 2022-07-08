2edf51c68faaea744bc3cec26dfeb7e8101614e1
frank <francisco.graya26@gmail.com>
2022-07-08 12:06:46 -0500

    inicio

---
+++ src/main/java/com/example/Meeseks/MeeseksApplication.java
@@ -0,0 +1,13 @@
+package com.example.Meeseks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeeseksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeeseksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Meeseks/MeeseksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Meeseks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeeseksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
