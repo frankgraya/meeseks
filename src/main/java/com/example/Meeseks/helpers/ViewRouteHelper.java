e1c04d4fda0edbba4f13e4d2f2bae0bb2b4474e1
frank <francisco.graya26@gmail.com>
2022-07-08 12:18:05 -0500

    Configuraciond de vistas y rutas

--- src/main/java/com/example/Meeseks/helpers/ViewRouteHelper.java
+++ src/main/java/com/example/Meeseks/helpers/ViewRouteHelper.java
@@ -1,4 +1,17 @@
 package com.example.Meeseks.helpers;
 
 public class ViewRouteHelper {
+
+
+    //home
+    public final static String INDEX = "index";
+
+    //Views
+    public final static String INDEX1 = "home/index";
+    public final static String HELLO = "home/hello";
+
+    //Redirects
+    public final static String ROUTE_INDEX = "/index";
+
+
 }
